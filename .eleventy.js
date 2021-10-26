f38218a0a6b7138e46382d8ff90ec873c84b934f
Dave Wilkinson <thinkdave@gmail.com>
2021-10-26 09:35:49 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(config) {
+
+    // Pass js files right through to Eleventy
+    config.addPassthroughCopy("src/js");
+
+    // Where to add input and output
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    };
+};
